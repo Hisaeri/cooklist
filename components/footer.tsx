1b8db8a6bde9359aa4f03adbb00a13eede741a97
Hisaeri <roch.fiona@gmail.com>
2022-02-02 21:34:17 +0900

    Add made in love by Hisaeri in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,12 @@
 const FooterComponent = ({}) => {
   return (
     <div>
-      <div className="container mx-auto">Footer</div>
+      <div className="container mx-auto">
+        {"Made with ❤️ by "}
+        <a target="_blank" href="https://github.com/Hisaeri">
+          Hisaeri
+        </a>
+      </div>
     </div>
   );
 };
