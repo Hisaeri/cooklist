2dad2d2b50d420f8bcd0b2622e5490ed36a187d1
Hisaeri <roch.fiona@gmail.com>
2022-02-02 21:17:02 +0900

    Use API server side props recipes for recipes page

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -1,41 +1,41 @@
-import { NextComponentType, NextPage } from "next";
+import {
+  GetStaticProps,
+  InferGetStaticPropsType,
+  NextComponentType,
+  NextPage,
+} from "next";
 import { Recipe } from "../types/recipes";
 import RecipesComponent from "../components/recipes";
 import Head from "next/head";
+import { apiUrl } from "../config";
 
-const RecipesPage: NextPage<NextComponentType> = () => {
-  const recipes: Recipe[] = [
-    {
-      id: 1,
-      name: "Tomato salad",
-      desc: "Simple salad, great in summer",
-      ingredients: [
-        { name: "tomato", quantity: 100 },
-        { name: "salad", quantity: 100 },
-      ],
-    },
-    {
-      id: 2,
-      name: "Tomato feta salad",
-      ingredients: [
-        { name: "tomato", quantity: 100 },
-        { name: "salad", quantity: 100 },
-        { name: "feta", quantity: 150 },
-      ],
-    },
-  ];
+export const getServerSideProps: GetStaticProps = async (context) => {
+  const res = await fetch(apiUrl + "/api/recipes");
+  const recipes: Recipe[] = await res.json();
 
+  return {
+    props: {
+      recipes,
+    },
+  };
+};
+
+const RecipesPage: NextPage<NextComponentType> = ({
+  recipes,
+}: InferGetStaticPropsType<typeof getServerSideProps>) => {
   return (
-    <div>
+    <>
       <Head>
         <title>Recipies | My Cooklist</title>
         <meta name="description" content="A list of custom recipies." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-5xl">Recipes</h1>
-      <RecipesComponent recipes={recipes} />
-    </div>
+      <div className="container mx-auto">
+        <h1 className="text-5xl">Recipes</h1>
+        <RecipesComponent recipes={recipes} />
+      </div>
+    </>
   );
 };
 
