a90102956e100a2752d5debd84ce819271c0e461
Hisaeri <roch.fiona@gmail.com>
2022-02-02 20:26:13 +0900

    Add app information on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,34 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>My Cooklist</title>
-        <meta name="description" content="Generate a groceries list from recipes." />
+        <meta
+          name="description"
+          content="Generate a groceries list from recipes."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
+        <h1>Welcome to Cooklist!</h1>
+        <h2>
+          Cooklist is an app made with NextJS and React that generates a
+          shopping list from selected recipes, for easy grocery shopping.
+        </h2>
+        <h3>
+          {"Checkout "}
+          <Link href="/recipes">
+            <a>My cooklist recipes</a>
+          </Link>
+        </h3>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
